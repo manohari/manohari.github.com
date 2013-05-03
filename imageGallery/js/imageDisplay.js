e6f2ef271bd563b8100e2d4dc97dd77fb1090805
manohari <v.manohari@gmail.com>
2013-05-03 09:16:40 +0530

    modularized code further

--- imageGallery/js/imageDisplay.js
+++ imageGallery/js/imageDisplay.js
@@ -1,12 +1,11 @@
 var imageGalleryView =  function(imgController) {
     "use strict";
+    var sec;
+    sec = document.getElementsByTagName("section")[0];
     return { 
         displayForm : function() {
             //this function creates files upload multiple element as well as drag and drop elements in html page
-            var sec,spanEle,spanText,spanText2,fileEle,secEle,divEle,innerDiv,outputEle;
-            
-            sec = document.getElementsByTagName("section")[0];
-            
+            var spanEle, spanText, spanText2, outputEle;
             spanEle = document.createElement("span");
             spanEle.className = 'notes';
             spanText = document.createTextNode("Please select any one option from below ");
@@ -20,6 +19,34 @@ var imageGalleryView =  function(imgController) {
             spanEle.appendChild(spanText2);
             sec.appendChild(spanEle);
             
+            this.addFileElement();
+            
+            spanEle = document.createElement("span");
+            spanEle.className = 'notes';
+            spanText = document.createTextNode("Drag and Drop");
+            spanEle.appendChild(spanText);
+            sec.appendChild(spanEle);
+            
+            this.addDragDropEle();
+            //html5 output element to display all uploaded as well as drag and drop elements
+            outputEle = document.createElement("output");
+            outputEle.id = 'list';
+            outputEle.addEventListener("click",imgController.handleLightBoxEvent,false);
+            sec.appendChild(outputEle);
+            
+        },
+        displayThumbNails : function (e) {
+            //showing uploaded images as thumbnails format
+            var span, image;
+            span = document.createElement('span');
+            image = document.createElement("img");
+            image.className = 'thumb';
+            image.src = e.target.result;
+            span.appendChild(image);
+            document.getElementById('list').insertBefore(span, null);
+        },
+        addFileElement : function() {
+            var secEle, fileEle;
             secEle = document.createElement("section");
             secEle.className = 'uploadFile';
             sec.appendChild(secEle);
@@ -35,83 +62,69 @@ var imageGalleryView =  function(imgController) {
                 imgController.handleImageEvents(e,0);
             }, false);
             secEle.appendChild(fileEle);
-            
-            spanEle = document.createElement("span");
-            spanEle.className = 'notes';
-            spanText = document.createTextNode("Drag and Drop");
-            spanEle.appendChild(spanText);
-            sec.appendChild(spanEle);
+        },
+        addDragDropEle : function() {
             //drag and drop div creations
+            var divEle, innerDiv;
             divEle = document.createElement("div");
             divEle.className = 'class="dragdrop">';
             sec.appendChild(divEle);
             //div inside above div
             innerDiv = document.createElement("div");
             innerDiv.id = 'drop_zone';
-            innerDiv.className = 'drop_zone';
+            innerDiv.className = 'dropZone';
             
             //event handlers for html5 drag and drop
             innerDiv.addEventListener('drop',function(e) {
                 imgController.handleImageEvents(e,1);
             }, false);
-            innerDiv.addEventListener('dragover',function(evt) {
-                evt.preventDefault();
-                evt.stopPropagation();
-                evt.dataTransfer.dropEffect = 'move';
-            }, false);
-            innerDiv.addEventListener('dragleave',function(evt) {
-                evt.preventDefault();
-                evt.stopPropagation();
-            }, false);
-            innerDiv.addEventListener('dragenter',function(evt) {
-                evt.preventDefault();
-                evt.stopPropagation();
-                evt.dataTransfer.effectAllowed = 'copyMove';
-            }, false);
+            innerDiv.addEventListener('dragover', imgController.handleMouseOverEvent, false);
+            innerDiv.addEventListener('dragleave', imgController.handleMouseLeaveEvent, false);
+            innerDiv.addEventListener('dragenter', imgController.handleMouseEnterEvent, false);
             
             innerDiv.appendChild(document.createTextNode('Drop files here'));
             divEle.appendChild(innerDiv);
-            
-            //html5 output element to display all uploaded as well as drag and drop elements
-            outputEle = document.createElement("output");
-            outputEle.id = 'list';
-            outputEle.addEventListener("click",imgController.handleLightBoxEvent,false);
-            sec.appendChild(outputEle);
-            
-        },
-        displayThumbNails : function (e) {
-            //showing uploaded images as thumbnails format
-            var span,image;
-            span = document.createElement('span');
-            image = document.createElement("img");
-            image.className = 'thumb';
-            image.src = e.target.result;
-            span.appendChild(image);
-            document.getElementById('list').insertBefore(span, null);
         }
     };
 };
 var imageGalleryModel = function () {
     return {
-        showLB : function(e) {
-            //light box API call to open lightbox and initiate image navigation
-            var evt,imgs,imgLoop,imgLength,lightbox,imageArray = [];
-            evt = e.target || e.srcElement;
-            imgs = document.getElementsByTagName("img");
-            imgLength = imgs.length;
-            for(imgLoop = 0; imgLoop < imgLength; imgLoop += 1) {
-                imgs[imgLoop].dataset.imgid = imgLoop;
-                imageArray[imgLoop] = imgs[imgLoop].src;
+        removeHandler : function (imgControllerObj, ele) {
+            var divEle, fileEle, outEle;
+            if(ele === 'drag') {
+                divEle = document.getElementById('drop_zone');
+                divEle.removeEventListener('drop', imgControllerObj.handleImageEvents, false);
+                divEle.removeEventListener('dragover', imgControllerObj.handleMouseOverEvent, false);
+                divEle.removeEventListener('dragenter', imgControllerObj.handleMouseEnterEvent, false);
+                divEle.removeEventListener('dragleave', imgControllerObj.handleMouseLeaveEvent, false);
             }
-            document.getElementById("overlayElement").style.display = 'block';
-            lightbox = new Lightbox('list');
-            lightbox.createOverlay(evt);
-        },
-        handleFileSelectEvent : function (e,opt) {
+            
+            //file handler removal
+            if(ele === 'file') {
+                fileEle = document.getElementsByTagName("input")[0];
+                if(fileEle.type === 'file') {
+                    fileEle.removeEventListener('change', imgControllerObj.handleImageEvents, false);
+                }
+            }
+            
+            if(ele === 'outEle') {
+                outEle = document.getElementById("list");
+                outEle.removeEventListener('click', imgControllerObj.handleLightBoxEvent, false)
+            }
+        } 
+    };
+};
+var imageGalleryController =  function() {
+    "use strict";
+    //controller display html view and handle business logic of image reading and showing
+    var viewForm, imgModel;
+    imgModel = imageGalleryModel();
+    return {
+        handleImageEvents : function(e,opt){
             //File Reader API of HTML5 used to read image and display
             e.stopPropagation();
             e.preventDefault();
-            var files,i,f,reader,otherFormat =0,span,image,images,thumbnails;
+            var files, loop, FileData, reader, otherFormat =0;
             if(opt === 0) {
                 //file api to read files when uploaded 
                 files = e.target.files; 
@@ -120,9 +133,9 @@ var imageGalleryModel = function () {
                 //drag and drop method file reading
                 files = e.dataTransfer.files;
             }
-            for (i = 0; i < files.length; i += 1) {
-                f = files[i];
-                if (!f.type.match('image.*')) {
+            for (loop = 0; loop < files.length; loop += 1) {
+                FileData = files[loop];
+                if (!FileData.type.match('image.*')) {
                     otherFormat += 1;
                     continue;
                 }
@@ -135,29 +148,50 @@ var imageGalleryModel = function () {
                          viewForm.displayThumbNails(e);
                      }
                  }
-                 reader.readAsDataURL(f);
+                 reader.readAsDataURL(FileData);
             }
             if(otherFormat > 1) {
                 alert('Only image files are allowed');
             }
-        }
-    };
-};
-var imageGalleryController =  function() {
-    "use strict";
-    //controller display html view and handle business logic of image reading and showing
-    var viewForm,imgModel;
-    imgModel = imageGalleryModel();
-    return {
-        handleImageEvents : function(evt,opt){
-            imgModel.handleFileSelectEvent(evt,opt);
+            if(opt === 0) {
+                imgModel.removeHandler(this,'file');
+            }
+            if(opt === 1) {
+                imgModel.removeHandler(this,'drag');
+            }
         },
         handleLightBoxEvent : function(e) {
-            imgModel.showLB(e);
+            //light box API call to open lightbox and initiate image navigation
+            var evt, imgs, imgLoop, imgLength, lightbox, imageArray = [];
+            evt = e.target || e.srcElement;
+            imgs = document.getElementsByTagName("img");
+            imgLength = imgs.length;
+            for(imgLoop = 0; imgLoop < imgLength; imgLoop += 1) {
+                imgs[imgLoop].dataset.imgid = imgLoop;
+                imageArray[imgLoop] = imgs[imgLoop].src;
+            }
+            imgModel.removeHandler(this,'outEle');
+            document.getElementById("overlayElement").style.display = 'block';
+            lightbox = new Lightbox('list');
+            lightbox.createOverlay(evt);
+        },
+        handleMouseOverEvent : function(evt) {
+                evt.preventDefault();
+                evt.stopPropagation();
+                evt.dataTransfer.dropEffect = 'move';
+        },
+        handleMouseLeaveEvent : function(evt) {
+                evt.preventDefault();
+                evt.stopPropagation();
+        },
+        handleMouseEnterEvent : function(evt) {
+                evt.preventDefault();
+                evt.stopPropagation();
+                evt.dataTransfer.effectAllowed = 'copyMove';
         },
         showForm : function() { 
             viewForm = imageGalleryView(this);
             viewForm.displayForm();
         }
-    };
+     };
 }; 
--- imageGallery/js/imageGallery_testCases.js
+++ imageGallery/js/imageGallery_testCases.js
@@ -1,4 +1,5 @@
-var ip,drag,img;
+var ip, drag, img, outputEle, imgDisplay, overlayEle;
+imgDisplay =new imageGalleryController();
 test("Image Gallery components", function() {
     ip = document.getElementsByTagName("input")[0].type;
     notEqual(ip, 'file','Input file tag not yet created');
@@ -9,13 +10,35 @@ test("Image Gallery components", function() {
 });
 
 test("Image Gallery components", function() {
-    var imgDisplay =new imageGalleryController();
     imgDisplay.showForm();
     ip = document.getElementById("files").type;
-    console.log(document.getElementById("files"));
     equal(ip, 'file','Input file tag created');
     drag = document.getElementById("drop_zone");
     notEqual(drag, 'drop_zone','Drag and drop created');
     img = document.getElementsByTagName("img")[0];
     equal(img,undefined,'Image Tags not yet exist');
+    
+    overlayEle = document.getElementById("overlayElement");
+    equal(overlayEle.className, 'overlayElement', 'Lightbox div exist');
+});
+test("Image Gallery components", function(e) {
+    var imgEle, outputEle, imgTag, imgTag, imgPath, imgModleObj, lightbox;
+    imgDisplay.showForm();
+    outputEle = document.getElementById('list');
+    notEqual(outputEle,undefined, "output tag exist");
+    imgPath = '../Fancybox/images/image-4.jpg';
+    imgTag = document.createElement("img");
+    imgTag.src = imgPath;
+    imgTag.className = "thumb";
+    imgTag.dataset.imgid = 0;
+    outputEle.appendChild(imgTag);
+    imgEle = document.getElementsByTagName("img")[0];
+    equal(imgEle.dataset.imgid,0, "one image is updated");
+    
+    imgModleObj = new imageGalleryModel();
+    imgDisplay.handleLightBoxEvent(e);
+    overlayEle = document.getElementById("overlayElement");
+    equal(overlayEle.style.display, 'block', "Lightbox layer shown");
+    lightbox = new Lightbox('list');
+    lightbox.createOverlay(imgEle);
 });
